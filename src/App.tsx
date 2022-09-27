f9fd2df4f2783eff9d2499698e62ae7ec3a9a500
PieroProcessing <piero.dauber@gmail.com>
2022-09-27 17:30:36 +0200

    render images on canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import { useCallback, useEffect, useState } from "react";
+import * as images from './assets/images';
 
 export const App = () => {
   const [canvas, setCanvas] = useState<HTMLCanvasElement>();
   const onRefCanvas = useCallback( (node: HTMLCanvasElement) => setCanvas(node), [])
+  const simulatedApi = () => Object.values(images);
+
   useEffect(() => {
     if (!canvas) return;
     const ctx = canvas.getContext('2d');
-  }, [canvas])
+    const imagesSrcList = simulatedApi();
+    const images = imagesSrcList.map(src => {
+      const IMG = new Image();
+      IMG.src = src;
+      return IMG
+    });
+ 
+    ctx && (ctx.fillStyle = "rgb(242, 242, 242)");
+    ctx?.fillRect(0, 0, canvas.width, canvas.height);
+    renderCanvas(images, 1, canvas, -200, ctx);
+    renderCanvas(images, 2, canvas, canvas.width * 1 - 200, ctx);
+    
+    
+  }, [canvas]);
+  
   return (
     <div className="slideshow">
       <header className="slideshow-header">
@@ -22,3 +39,28 @@ export const App = () => {
 }
 
 export default App;
+
+const scaleForImage =  ( image: HTMLImageElement, canvas: HTMLCanvasElement) => {
+  const aspect =  canvas.width / canvas.height;
+  const ratio =  image.width / image.height;
+  return ratio >= aspect 
+    ? canvas.width / image.width
+    : aspect > ratio 
+      ? canvas.height / image.height
+      : canvas.width
+  
+}
+ 
+ 
+const indecesForOffset =  (offset: number, numImg: number, sceneWidth: number) => {
+  return [Math.floor(offset / sceneWidth * numImg), Math.ceil(offset / sceneWidth * numImg)]
+};
+
+function renderCanvas(images: HTMLImageElement[], index: number, canvas: HTMLCanvasElement, offset: number, ctx: CanvasRenderingContext2D | null) {
+  const width = images[index].width * scaleForImage(images[index], canvas);
+  const height = images[index].height * scaleForImage(images[index], canvas);
+  const x = offset + (canvas.width - width) / 2;
+  const y = (canvas.height - height) / 2;
+
+  ctx?.drawImage(images[index], Math.round(x), Math.round(y), Math.round(width), Math.round(height));
+}
---
+++ src/assets/images/index.ts
@@ -0,0 +1,11 @@
+import image0 from './0.jpeg'
+import image1 from './1.jpeg'
+import image2 from './2.jpeg'
+import image3 from './3.jpeg'
+
+export {
+    image0,
+    image1,
+    image2,
+    image3
+}
