84737da13d4e380c510ccca764d4b2317d91749a
PieroProcessing <piero.dauber@gmail.com>
2022-09-29 01:47:39 +0200

    refactoring draw canvas and offset values

--- src/components/Canvas/useCanvasHook.tsx
+++ src/components/Canvas/useCanvasHook.tsx
@@ -1,9 +1,9 @@
 import { MouseEventHandler, useCallback, useEffect, useState, useRef } from "react";
 import { simulatedApi } from "../../services";
-import { getMousePositionOnCanvas, indecesForOffset, scaleForImage, updateOffset } from "../../utils";
+import { drawCanvas, getMousePositionOnCanvas, updateOffset } from "../../utils";
 
 export const useCanvasHook = () => {
-    const  [images, setImages] = useState<HTMLImageElement[]>();
+    const [images, setImages] = useState<HTMLImageElement[]>();
     const [canvas, setCanvas] = useState<HTMLCanvasElement>();
     const isGrabbing = useRef<boolean>(false);
     const mousePostion = useRef({
@@ -13,54 +13,31 @@ export const useCanvasHook = () => {
 
     const onRefCanvas = useCallback((node: HTMLCanvasElement) => setCanvas(node), [])
 
-    const renderCanvas = useCallback(( offset: number) => {
-        if(!canvas || !images?.length)return;
-       
-        const ctx = canvas.getContext('2d');
-        ctx && (ctx.fillStyle = "rgb(242, 242, 242)");
-        ctx?.fillRect(0, 0, canvas.width, canvas.height);
-
-        const indeces = indecesForOffset(offset, images.length, images.length * canvas.width);
-
-        const callback = (index: number, image: HTMLImageElement) => {
-            const width = image.width * scaleForImage(image, canvas);
-            const height = image.height * scaleForImage(image, canvas);
-            const x = (canvas.width * index - offset) + (canvas.width - width) / 2;
-            const y = (canvas.height - height) / 2;
-            ctx?.drawImage(image, Math.round(x), Math.round(y), Math.round(width), Math.round(height),)
-        }
-        
-        indeces.forEach( index => callback(index, images[index]))
-
-    },[canvas, images]);
-    
-    useEffect(()=>{
+    useEffect(() => {
         simulatedApi()
             .then(setImages)
             .catch(console.error)
-    },[])
+    }, [])
     useEffect(() => {
-        renderCanvas(mousePostion.current.actual);
-    }, [ renderCanvas]);
-   
+        if (!canvas || !images?.length) return;
+        drawCanvas(canvas, mousePostion.current.actual, images);
+    }, [canvas, images]);
+
 
     const onDrag: MouseEventHandler<HTMLCanvasElement> = useCallback((event) => {
 
         if (!canvas || !images?.length) return
         if (!isGrabbing.current) return;
 
-
-        const updatePosition = updateOffset(canvas, mousePostion, images )
-        const { x } = getMousePositionOnCanvas(canvas, event);
-        const offset = updatePosition(x);
-
+        const actual = getMousePositionOnCanvas(canvas, event);
+        const offset = updateOffset(canvas, mousePostion, images)(actual);
         if (!offset) return;
 
         mousePostion.current.actual = offset;
-        renderCanvas(mousePostion.current.actual);
-        mousePostion.current.prevPosition = x;
-   
-    }, [canvas, images, renderCanvas]);
+        drawCanvas(canvas, mousePostion.current.actual, images);
+        mousePostion.current.prevPosition = actual;
+
+    }, [canvas, images]);
 
     const onCursor = useCallback((type: 'grab' | 'active', action?: 'up' | 'down'): MouseEventHandler<HTMLCanvasElement> =>
         (event) => {
@@ -72,9 +49,12 @@ export const useCanvasHook = () => {
                 : (event.target as HTMLCanvasElement)?.classList.add(type);
 
             isGrabbing.current = type === 'grab' && action === 'down';
+            !isGrabbing.current
+                && el.classList.contains('grab')
+                && (event.target as HTMLCanvasElement)?.classList.remove('grab');
 
             isGrabbing.current
-                ? (mousePostion.current.prevPosition = getMousePositionOnCanvas(canvas, event).x)
+                ? (mousePostion.current.prevPosition = getMousePositionOnCanvas(canvas, event))
                 : (mousePostion.current.prevPosition = 0)
 
         }, [canvas]);
@@ -83,5 +63,7 @@ export const useCanvasHook = () => {
         onRefCanvas, onDrag, onCursor
     }
 }
+
+
 
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { MouseEvent, MutableRefObject } from "react";
 
-export const scaleForImage = (image: HTMLImageElement, canvas: HTMLCanvasElement) => {
+const scaleForImage = (image: HTMLImageElement, canvas: HTMLCanvasElement) => {
     const aspect = canvas.width / canvas.height;
     const ratio = image.width / image.height;
     return ratio >= aspect
@@ -8,16 +8,13 @@ export const scaleForImage = (image: HTMLImageElement, canvas: HTMLCanvasElement
         : canvas.height / image.height
 };
 
-export const indecesForOffset = (offset: number, numImg: number, sceneWidth: number) => {
+const indecesForOffset = (offset: number, numImg: number, sceneWidth: number) => {
     return [Math.floor(offset / sceneWidth * numImg), Math.ceil(offset / sceneWidth * numImg)]
 };
 
 export const getMousePositionOnCanvas = (canvas: HTMLCanvasElement, event: MouseEvent) => {
     const rect = canvas.getBoundingClientRect();
-    return {
-        x: event.clientX - rect.left,
-        y: event.clientY - rect.top
-    };
+    return  event.clientX - rect.left
 };
  
 export const updateOffset = (
@@ -26,11 +23,25 @@ export const updateOffset = (
     images: HTMLImageElement[]) =>
     (x: number) => {
         const sceneWidth = canvas.width * images.length;
-
         const { prevPosition, actual } = mousePostion.current;
         const newPos = actual + (prevPosition - x);
-        const offset = (newPos >= 0 && newPos <= sceneWidth - canvas.width && newPos)
-            || (newPos < 0 && 0)
-            || (newPos > sceneWidth - canvas.width && sceneWidth - canvas.width);
+        const offset = Math.max(0, newPos) && Math.min(newPos, sceneWidth - canvas.width)
         return offset;
     }
+export const drawCanvas = (canvas: HTMLCanvasElement, offset: number, images: HTMLImageElement[]) => {
+    const ctx = canvas.getContext('2d');
+    ctx && (ctx.fillStyle = "rgb(242, 242, 242)");
+    ctx?.fillRect(0, 0, canvas.width, canvas.height);
+
+    const indeces = indecesForOffset(offset, images.length, images.length * canvas.width);
+
+    const callback = (index: number, image: HTMLImageElement) => {
+        const width = image.width * scaleForImage(image, canvas);
+        const height = image.height * scaleForImage(image, canvas);
+        const x = (canvas.width * index - offset) + (canvas.width - width) / 2;
+        const y = (canvas.height - height) / 2;
+        ctx?.drawImage(image, Math.round(x), Math.round(y), Math.round(width), Math.round(height));
+    };
+
+    indeces.forEach(index => callback(index, images[index]));
+}
